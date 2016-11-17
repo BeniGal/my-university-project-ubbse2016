1dda381ad258264b5367f92b282bec84c84ae340
Béni Gál <Beni.Gal@ngi.systems>
2016-11-17 13:58:22 +0200

    Interacting with layout elements from the activity

--- VirtualAssistantAndroidClient/app/src/main/java/fakanal/android/MainActivity.java
+++ VirtualAssistantAndroidClient/app/src/main/java/fakanal/android/MainActivity.java
@@ -3,6 +3,10 @@ package fakanal.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import fakanal.android.network.ServerAPI;
 import fakanal.android.network.ServerAPIBuilder;
@@ -14,16 +18,30 @@ public class MainActivity extends AppCompatActivity implements Callback<Void> {
     private static final String TAG = MainActivity.class.getName();
     public static ServerAPI API;
 
+    private final Button sendButton = (Button) findViewById(R.id.button);
+    private final EditText editText = (EditText) findViewById(R.id.editText);
+    private final TextView textView = (TextView) findViewById(R.id.textView);
+
+    // Address is hardcoded for now, later may be injected.
+    private String serverAddress = "35.156.26.234";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Address is hardcoded for now, later may be injected.
-        String serverAddress = "1.2.3.4";
+        final Callback<Void> thiz = this;
 
-        API = new ServerAPIBuilder(serverAddress).build();
-        Call<Void> helloWorldApiCall = API.helloWorldCall("Hello World message to the server");
-        helloWorldApiCall.enqueue(this);
+        // Simple click listener
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String message = String.valueOf(editText.getText());
+                Log.i(TAG, "Sending request to server with message: " + message);
+
+                API = new ServerAPIBuilder(serverAddress).build();
+                Call<Void> helloWorldApiCall = API.helloWorldCall(message);
+                helloWorldApiCall.enqueue(thiz);
+            }
+        });
 
         setContentView(R.layout.activity_main);
     }
@@ -31,14 +49,17 @@ public class MainActivity extends AppCompatActivity implements Callback<Void> {
     @Override
     public void onResponse(Call<Void> helloWorldApiCall, Response<Void> response) {
         if (response.isSuccessful()) {
-            Log.i(TAG, "Successfully got answer from the server, response: " + response.code());
+            Log.i(TAG, "Successfully got answer from the server, response: " + response);
+            textView.setText(response.message());
         } else {
             Log.e(TAG, "Failed to get a success answer from the server, response: " + response.code());
+            textView.setText(":(");
         }
     }
 
     @Override
     public void onFailure(Call<Void> helloWorldApiCall, Throwable t) {
         Log.e(TAG, "Failed to get answer from the server", t);
+        textView.setText(":((");
     }
 }
